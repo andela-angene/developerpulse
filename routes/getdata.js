6b5916ebfb09b01d0c27e81c265a2714cd417923
Anthony Ngene <anthony.ngene@andela.com>
2017-11-12 21:35:19 +0100

    setup the backend to use google sheet api

---
+++ routes/getdata.js
@@ -0,0 +1,67 @@
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
+
+// const redirectUris = 'urn:ietf:wg:oauth:2.0:oob';
+
+const authDetails = {
+  installed: {
+    client_id: process.env.client_id,
+    client_secret: process.env.client_secret,
+    redirect_uris: process.env.redirect_uris,
+  },
+};
+
+function authorize(credentials, response, callback) {
+  const clientSecret = credentials.installed.client_secret;
+  const clientId = credentials.installed.client_id;
+  const redirectUrl = credentials.installed.redirect_uris;
+  const auth = new googleAuth();
+  const oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  oauth2Client.credentials = {
+    access_token: process.env.access_token,
+    refresh_token: process.env.refresh_token,
+    token_type: process.env.token_type,
+    expiry_date: process.env.expiry_date,
+  };
+  callback(oauth2Client, response);
+}
+
+function render(auth, details) {
+  const sheets = google.sheets('v4');
+  sheets.spreadsheets.values.get(
+    {
+      auth,
+      spreadsheetId: '1lGOhWZN3ENtDiKqmywayhUxCh7SGUJSi0Z47CP1nOEY',
+      range: 'Report (Final) - Individual Che!A2:X',
+    },
+    (err, response) => {
+      if (err) {
+        console.log(`The API returned an error: ${err}`);
+        return details.res.redirect('/login');
+      }
+      const rows = response.values;
+
+      let filtered = rows.filter(row => row[12] === details.userMail && row[8] === details.userId);
+
+      filtered = filtered.map(row => ({
+        date: row[0],
+        staff: `${row[4]} ${row[5]}`,
+        client: row[13],
+        fellow: row[9],
+        ratings: row[18],
+      }));
+
+      return details.res.render('index', {
+        title: 'Fellow Status',
+        filtered,
+      });
+    },
+  );
+}
+
+module.exports = {
+  authDetails,
+  authorize,
+  render,
+};
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const sheet = require('./getdata');
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  const { token } = req.query;
+
+  if (token) {
+    const decoded = jwt.decode(token);
+    if (decoded) {
+      const details = {
+        userMail: decoded.UserInfo.email,
+        userId: decoded.UserInfo.id,
+        res,
+      };
+
+      return sheet.authorize(sheet.authDetails, details, sheet.render);
+    }
+  }
+
+  return res.redirect('/login');
+});
+
+/* GET login page. */
+router.get('/login', (req, res) => {
+  res.render('login', {
+    title: 'Fellow Status - Login',
+  });
+});
+
+module.exports = router;
